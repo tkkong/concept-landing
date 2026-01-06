382051711fa064ee2fada2f0f0fa1587beb4b810
tkkong <tkong98@gmail.com>
2026-01-05 19:08:26 -0500

    initializing

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="concept-text">CONCEPT</h1>
+      <div className="links-container">
+        <a href="https://x.com/conceptdotdev" className="link" target="_blank" rel="noopener noreferrer">X</a>
+        <a href="https://linkedin.com/company/concept-dev" className="link" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="mailto:hello@concept.dev" className="link">Contact</a>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
