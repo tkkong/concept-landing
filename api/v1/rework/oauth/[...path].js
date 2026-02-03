6122975936ae1a7f9b7022bdaa7fca49eff0f814
Phil Chen <philchen2000@gmail.com>
2026-02-03 01:22:46 -0800

    fix

--- api/v1/rework/oauth/[...path].js
+++ api/v1/rework/oauth/[...path].js
@@ -7,11 +7,6 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  const originalUrl = req.url || '';
-  if (originalUrl.startsWith('/api/v1/rework/oauth')) {
-    req.url = originalUrl.replace(/^\/api\/v1/, '');
-  }
-
   const handled = await broker.handle(req, res);
   if (!handled && !res.writableEnded) {
     res.statusCode = 404;
